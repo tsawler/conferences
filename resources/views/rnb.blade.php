1d95aaff99422ac350410648e45d9a53aee80cc5
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-09 16:40:13 -0300

    Client additions

--- resources/views/rnb.blade.php
+++ resources/views/rnb.blade.php
@@ -1,44 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head profile="http://gmpg.org/xfn/11">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <!-- METATAGS -->
     <title>Welcome - Recycle NB</title>
 
-    <meta name='description' content='' />
-    <meta name='keywords' content='' />
-    <meta name='robots' content='index,follow,archive' />
+    <meta name='description' content=''/>
+    <meta name='keywords' content=''/>
+    <meta name='robots' content='index,follow,archive'/>
 
-    <meta name='geo.position' content=',' />
-    <meta name='geo.placename' content='' />
-    <meta name='geo.region' content='' />
+    <meta name='geo.position' content=','/>
+    <meta name='geo.placename' content=''/>
+    <meta name='geo.region' content=''/>
 
 
-    <link rel='schema.DC' href='http://purl.org/dc/elements/1.1/' />
-    <link rel='schema.DCTERMS' href='http://purl.org/dc/terms/' />
-    <meta name='DC.title' content='Welcome - Recycle NB' />
-    <meta name='DC.creator' content='Administrator' />
-    <meta name='DC.subject' content='' />
-    <meta name='DC.description' content='' />
-    <meta name='DC.publisher' content='' />
-    <meta name='DC.date.created' scheme='DCTERMS.W3CDTF' content='2010-06-23T11:19:28-0400' />
-    <meta name='DC.date.modified' scheme='DCTERMS.W3CDTF' content='2010-11-25T16:07:29-0500' />
-    <meta name='DC.date.valid' scheme='DCTERMS.W3CDTF' content='2015-04-15T16:38:56-0400' />
-    <meta name='DC.type' scheme='DCTERMS.DCMIType' content='Text' />
-    <meta name='DC.rights' scheme='DCTERMS.URI' content='' />
-    <meta name='DC.format' content='text/html' />
-    <meta name='DC.identifier' scheme='DCTERMS.URI' content='http://www.recyclenb.com/en/en/' />
+    <link rel='schema.DC' href='http://purl.org/dc/elements/1.1/'/>
+    <link rel='schema.DCTERMS' href='http://purl.org/dc/terms/'/>
+    <meta name='DC.title' content='Welcome - Recycle NB'/>
+    <meta name='DC.creator' content='Administrator'/>
+    <meta name='DC.subject' content=''/>
+    <meta name='DC.description' content=''/>
+    <meta name='DC.publisher' content=''/>
+    <meta name='DC.date.created' scheme='DCTERMS.W3CDTF' content='2010-06-23T11:19:28-0400'/>
+    <meta name='DC.date.modified' scheme='DCTERMS.W3CDTF' content='2010-11-25T16:07:29-0500'/>
+    <meta name='DC.date.valid' scheme='DCTERMS.W3CDTF' content='2015-04-15T16:38:56-0400'/>
+    <meta name='DC.type' scheme='DCTERMS.DCMIType' content='Text'/>
+    <meta name='DC.rights' scheme='DCTERMS.URI' content=''/>
+    <meta name='DC.format' content='text/html'/>
+    <meta name='DC.identifier' scheme='DCTERMS.URI' content='http://www.recyclenb.com/en/en/'/>
 
     <!-- STYLESHEETS -->
-    <link rel="stylesheet" href="/resource/css/screen.css" type="text/css" media="screen,projection" />
+    <link rel="stylesheet" href="/resource/css/screen.css" type="text/css" media="screen,projection"/>
 
-    <!--[if IE 8]><link rel="stylesheet" href="/resource/css/lib/ie8.css" type="text/css" media="screen,projection" /><![endif]-->
-    <!--[if IE 7]><link rel="stylesheet" href="/resource/css/lib/ie7.css" type="text/css" media="screen,projection" /><![endif]-->
-    <!--[if IE 6]><link rel="stylesheet" href="/resource/css/lib/ie6.css" type="text/css" media="screen,projection" /><![endif]-->
+    <!--[if IE 8]>
+    <link rel="stylesheet" href="/resource/css/lib/ie8.css" type="text/css" media="screen,projection"/><![endif]-->
+    <!--[if IE 7]>
+    <link rel="stylesheet" href="/resource/css/lib/ie7.css" type="text/css" media="screen,projection"/><![endif]-->
+    <!--[if IE 6]>
+    <link rel="stylesheet" href="/resource/css/lib/ie6.css" type="text/css" media="screen,projection"/><![endif]-->
 
     <!-- PRINT STYLESHEET -->
-    <link rel="stylesheet" href="/resource/css/print.css" type="text/css" media="print" />
+    <link rel="stylesheet" href="/resource/css/print.css" type="text/css" media="print"/>
 
     <link rel="shortcut icon" type="image/x-icon" href="/resource/icons/favicon.ico">
 
@@ -57,10 +61,12 @@
         input {
             width: 50%;
         }
+
         label {
             margin-bottom: 15px;
             display: block;
         }
+
         select {
             width: 50%;
         }
@@ -71,7 +77,6 @@
 
 <!-- HEADER -->
 <div id="header">
-
 
 
     <!-- LANGUAGE -->
@@ -111,65 +116,87 @@
     <div id="wrapper" class="clearfix  ">
 
 
-
         <!-- LEFTCOL -->
         <div id="leftCol">
 
-            <h1>Conference Registration</h1>
+            <h2>Moving Beyond Waste:</h2>
+
+            <h3>Waste Packaging and Printed Paper Forum</h3>
+
+            <h3>May 28, 2015 - Crowne Plaza Hotel, Fredericton</h3>
+
+            <p>Packaging and Printed Paper (PPP) make up a large portion of the residential waste stream in every
+                jurisdiction in Canada. Manitoba, Ontario, Quebec and British Columbia have packaging and printed paper
+                stewardship programs that industry finances.</p>
+
+            <p>At the Waste Packaging and Printed Paper Forum, you will learn about what a PPP program would mean for
+                municipalities in New Brunswick, funding models, and have an opportunity to talk to representatives from
+                provinces that have EPR programs for these products.</p>
+
+            <p>HOTEL: Crowne Plaza is holding guestrooms, at the government rate, until April 28, 2015. If you wish to
+                book a room call 1-866-444-1946.</p>
+            <hr>
+
+            <br>
 
             <form method="post" name="regform" id="regform">
 
-                <label for="title">Title<br>
-                    <input type="text" name="title">
-                </label>
+                <fieldset>
+                    <legend>Registration Information</legend>
+                    <br>
+                    <label for="title">Title<br>
+                        <input type="text" name="title">
+                    </label>
 
-                <label for="first_name">First Name<br>
-                    <input type="text" name="first_name">
-                </label>
+                    <label for="first_name">First Name<br>
+                        <input type="text" name="first_name">
+                    </label>
 
-                <label for="last_name">Last Name<br>
-                    <input type="text" name="first_name">
-                </label>
+                    <label for="last_name">Last Name<br>
+                        <input type="text" name="first_name">
+                    </label>
 
-                <label for="company">Company/Organization<br>
-                    <input type="text" name="first_name">
-                </label>
+                    <label for="company">Company/Organization<br>
+                        <input type="text" name="first_name">
+                    </label>
 
-                <label for="email">Email address<br>
-                    <input type="email" name="first_name">
-                </label>
+                    <label for="email">Email address<br>
+                        <input type="email" name="first_name">
+                    </label>
 
-                <label for="address">Mailing address<br>
-                    <input type="text" name="address">
-                </label>
+                    <label for="address">Mailing address<br>
+                        <input type="text" name="address">
+                    </label>
 
-                <label for="city">City/Town<br>
-                    <input type="text" name="city">
-                </label>
+                    <label for="city">City/Town<br>
+                        <input type="text" name="city">
+                    </label>
 
-                <label for="zip">Postal Code<br>
-                    <input type="text" name="zip">
-                </label>
+                    <label for="zip">Postal Code<br>
+                        <input type="text" name="zip">
+                    </label>
 
-                <label for="phone">Phone<br>
-                    <input type="text" name="phone">
-                </label>
+                    <label for="phone">Phone<br>
+                        <input type="text" name="phone">
+                    </label>
 
-                <label for="commission">Solid Waste Commission<br>
-                    <select name="commission">
-                        <option value="">N/A</option>
-                    </select>
-                </label>
+                    <label for="commission">Solid Waste Commission<br>
+                        <select name="commission">
+                            <option value="">N/A</option>
+                        </select>
+                    </label>
 
-                <label for="regional_commission">Regional Service Commission<br>
-                    <select name="regional_commission">
-                        <option value="">N/A</option>
-                    </select>
-                </label>
+                    <label for="regional_commission">Regional Service Commission<br>
+                        <select name="regional_commission">
+                            <option value="">N/A</option>
+                        </select>
+                    </label>
+
+                </fieldset>
 
                 <hr>
                 <p>You may wish to review our <a href="">privacy policy</a>. We will never share
-                your personal information with a third party, and only use it to organize the conference.</p>
+                    your personal information with a third party, and only use it to organize the conference.</p>
                 <button>Submit Registration</button>
 
             </form>
@@ -198,14 +225,14 @@
 
             {{--<div id="survey">--}}
 
-                {{--<h3></h3>--}}
+            {{--<h3></h3>--}}
 
-                {{--<p style="margin-top: -30px; padding-right: 20px;">--}}
-                    {{--<strong>Program Registration Forms</strong><br />--}}
-                    {{--&bull; <a href="http://www.recyclenb.com/en/docs/Brand-Owner-Registration-Form-ENG.pdf" target="_blank">Oil and Glycol Program</a><br />--}}
-                    {{--&bull; <a href="http://www.recyclenb.com/en/docs/RNB-Registration-Form-Tire-Program.pdf" target="_blank">Tire Program</a><br />--}}
-                    {{--&bull; <a href="http://www.recyclenb.com/en/docs/Brand-Owner-Registration-Form-ENG.pdf" target="_blank">Paint Program</a>--}}
-                {{--</p>--}}
+            {{--<p style="margin-top: -30px; padding-right: 20px;">--}}
+            {{--<strong>Program Registration Forms</strong><br />--}}
+            {{--&bull; <a href="http://www.recyclenb.com/en/docs/Brand-Owner-Registration-Form-ENG.pdf" target="_blank">Oil and Glycol Program</a><br />--}}
+            {{--&bull; <a href="http://www.recyclenb.com/en/docs/RNB-Registration-Form-Tire-Program.pdf" target="_blank">Tire Program</a><br />--}}
+            {{--&bull; <a href="http://www.recyclenb.com/en/docs/Brand-Owner-Registration-Form-ENG.pdf" target="_blank">Paint Program</a>--}}
+            {{--</p>--}}
 
             {{--</div>--}}
 
@@ -215,18 +242,14 @@
             {{--<!-- CAREER OPPORTUNITIES -->--}}
             {{--<div id="careerOpps">--}}
 
-                {{--<h3>Career Opportunities</h3>--}}
+            {{--<h3>Career Opportunities</h3>--}}
 
-                {{--<ul>--}}
-                    {{--<li class="last"><p>There are no career opportunities available at the present time.</p></li>--}}
-                {{--</ul>--}}
+            {{--<ul>--}}
+            {{--<li class="last"><p>There are no career opportunities available at the present time.</p></li>--}}
+            {{--</ul>--}}
 
             {{--</div>--}}
             <!-- /CAREER OPPORTUNITIES -->
-
-
-
-
 
 
         </div>
@@ -268,15 +291,19 @@
             <li>
                 <ul>
                     <li><a href="http://www.recyclenb.com/en/programs/tire/" title="Tire Program">Tire Program</a></li>
-                    <li><a href="http://www.recyclenb.com/en/programs/paint/" title="Paint Program (EPR)">Paint Program (EPR)</a></li>
-                    <li><a href="http://www.recyclenb.com/en/programs/oil/" title="Oil and Glycol Program (EPR)">Oil and Glycol Program (EPR)</a></li>
+                    <li><a href="http://www.recyclenb.com/en/programs/paint/" title="Paint Program (EPR)">Paint Program
+                            (EPR)</a></li>
+                    <li><a href="http://www.recyclenb.com/en/programs/oil/" title="Oil and Glycol Program (EPR)">Oil and
+                            Glycol Program (EPR)</a></li>
                 </ul>
             </li>
             <li>
                 <ul>
                     <li><a href="http://www.recyclenb.com/en/kids/" title="Kid's Page">Kid's Page</a></li>
-                    <li><a href="http://www.recyclenb.com/en/teachers_resources/" title="Teacher's Resources">Teacher's Resources</a></li>
-                    <li><a href="http://www.recyclenb.com/en/recycling_dictionary/" title="Recycling Dictionary">Recycling Dictionary</a></li>
+                    <li><a href="http://www.recyclenb.com/en/teachers_resources/" title="Teacher's Resources">Teacher's
+                            Resources</a></li>
+                    <li><a href="http://www.recyclenb.com/en/recycling_dictionary/" title="Recycling Dictionary">Recycling
+                            Dictionary</a></li>
                 </ul>
             </li>
             <li>
@@ -302,19 +329,18 @@
 <!-- /FOOTERWRAPPER -->
 
 
-
 <!-- GA -->
 {{--<script type="text/javascript">--}}
 
-    {{--var _gaq = _gaq || [];--}}
-    {{--_gaq.push(['_setAccount', 'UA-36229613-1']);--}}
-    {{--_gaq.push(['_trackPageview']);--}}
+{{--var _gaq = _gaq || [];--}}
+{{--_gaq.push(['_setAccount', 'UA-36229613-1']);--}}
+{{--_gaq.push(['_trackPageview']);--}}
 
-    {{--(function() {--}}
-        {{--var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;--}}
-        {{--ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';--}}
-        {{--var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);--}}
-    {{--})();--}}
+{{--(function() {--}}
+{{--var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;--}}
+{{--ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';--}}
+{{--var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);--}}
+{{--})();--}}
 
 {{--</script>--}}
 
