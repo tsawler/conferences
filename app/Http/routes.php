1bbde2561f72dbf7786181fcf614976a83ecda7e
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-09 23:19:51 -0300

    push queues

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,6 +27,10 @@
 Route::get('/', 'ConferenceRegistrationController@getRegistrationform');
 Route::post('/register', 'ConferenceRegistrationController@postRegistrationform');
 
+Route::post('/queue/push', function(){
+    return Queue::marshal();
+});
+
 Route::controllers([
     'auth'     => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
