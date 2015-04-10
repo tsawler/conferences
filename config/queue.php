51a1d07f76e5dd278611c2266507d3b5046ea039
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-09 23:16:18 -0300

    push queues

--- config/queue.php
+++ config/queue.php
@@ -60,9 +60,9 @@ return [
 		'iron' => [
 			'driver'  => 'iron',
 			'host'    => 'mq-aws-us-east-1.iron.io',
-			'token'   => 'your-token',
-			'project' => 'your-project-id',
-			'queue'   => 'your-queue-name',
+			'token'   => '0OLjKTVrk_jrCGIOL8yqrL17vvQ',
+			'project' => '552731f8711c76000900003e',
+			'queue'   => 'recyclequeue',
 			'encrypt' => true,
 		],
 
