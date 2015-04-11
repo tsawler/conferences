5bc2a7663a8dcc07a2bbf5c7f122670844c431eb
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-11 09:30:51 -0300

    Formatting

--- resources/views/rnb.blade.php
+++ resources/views/rnb.blade.php
@@ -1,73 +1,41 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<!DOCTYPE html>
+<html lang="en">
 <head profile="http://gmpg.org/xfn/11">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- METATAGS -->
     <title>Welcome - Recycle NB</title>
 
-    <meta name='description' content=''/>
-    <meta name='keywords' content=''/>
-    <meta name='robots' content='index,follow,archive'/>
-
-    <meta name='geo.position' content=','/>
-    <meta name='geo.placename' content=''/>
-    <meta name='geo.region' content=''/>
-
-
-    <link rel='schema.DC' href='http://purl.org/dc/elements/1.1/'/>
-    <link rel='schema.DCTERMS' href='http://purl.org/dc/terms/'/>
-    <meta name='DC.title' content='Welcome - Recycle NB'/>
-    <meta name='DC.creator' content='Administrator'/>
-    <meta name='DC.subject' content=''/>
-    <meta name='DC.description' content=''/>
-    <meta name='DC.publisher' content=''/>
-    <meta name='DC.date.created' scheme='DCTERMS.W3CDTF' content='2010-06-23T11:19:28-0400'/>
-    <meta name='DC.date.modified' scheme='DCTERMS.W3CDTF' content='2010-11-25T16:07:29-0500'/>
-    <meta name='DC.date.valid' scheme='DCTERMS.W3CDTF' content='2015-04-15T16:38:56-0400'/>
-    <meta name='DC.type' scheme='DCTERMS.DCMIType' content='Text'/>
-    <meta name='DC.rights' scheme='DCTERMS.URI' content=''/>
-    <meta name='DC.format' content='text/html'/>
-    <meta name='DC.identifier' scheme='DCTERMS.URI' content='http://www.recyclenb.com/en/en/'/>
+    <meta name='description' content=''>
+    <meta name='keywords' content=''>
+    <meta name='robots' content='index,follow,archive'>
 
     <!-- STYLESHEETS -->
-    <link rel="stylesheet" href="/resource/css/screen.css" type="text/css" media="screen,projection"/>
-
+    <link rel="stylesheet" href="/resource/css/screen.css" type="text/css" media="screen,projection">
     <!--[if IE 8]>
-    <link rel="stylesheet" href="/resource/css/lib/ie8.css" type="text/css" media="screen,projection"/><![endif]-->
+    <link rel="stylesheet" href="/resource/css/lib/ie8.css" type="text/css" media="screen,projection"><![endif]-->
     <!--[if IE 7]>
-    <link rel="stylesheet" href="/resource/css/lib/ie7.css" type="text/css" media="screen,projection"/><![endif]-->
+    <link rel="stylesheet" href="/resource/css/lib/ie7.css" type="text/css" media="screen,projection"><![endif]-->
     <!--[if IE 6]>
-    <link rel="stylesheet" href="/resource/css/lib/ie6.css" type="text/css" media="screen,projection"/><![endif]-->
-
+    <link rel="stylesheet" href="/resource/css/lib/ie6.css" type="text/css" media="screen,projection"><![endif]-->
     <!-- PRINT STYLESHEET -->
-    <link rel="stylesheet" href="/resource/css/print.css" type="text/css" media="print"/>
-
+    <link rel="stylesheet" href="/resource/css/print.css" type="text/css" media="print">
     <link rel="shortcut icon" type="image/x-icon" href="/resource/icons/favicon.ico">
-
     <!--[if IE 6]>
     <script type="text/javascript" src="/resource/js/ie6.js"></script>
     <![endif]-->
-
-    <!-- JAVASCRIPT LIBRARIES -->
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
-
     <style>
         input {
             width: 50%;
         }
-
         label {
             margin-bottom: 15px;
             display: block;
         }
-
         select {
             width: 50%;
         }
-
     </style>
 
 </head>
@@ -283,26 +251,25 @@
 </div>
 <!-- /FOOTERWRAPPER -->
 
-
-<!-- GA -->
-{{--<script type="text/javascript">--}}
-
-{{--var _gaq = _gaq || [];--}}
-{{--_gaq.push(['_setAccount', 'UA-36229613-1']);--}}
-{{--_gaq.push(['_trackPageview']);--}}
-
-{{--(function() {--}}
-{{--var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;--}}
-{{--ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';--}}
-{{--var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);--}}
-{{--})();--}}
-
-{{--</script>--}}
+<!-- JAVASCRIPT LIBRARIES -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
 
 <script>
     $(document).ready(function () {
         $("#regform").validate();
     });
+</script>
+
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36229613-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 </script>
 </body>
 </html>
