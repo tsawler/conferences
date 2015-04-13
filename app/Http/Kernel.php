74c841b4edd8886c76f7b7ef10dfbc91f6d4f998
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-13 15:20:37 -0300

    Middleware addition

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,18 +26,19 @@ class Kernel extends HttpKernel {
      * @var array
      */
     protected $routeMiddleware = [
-        'auth'           => 'App\Http\Middleware\Authenticate',
-        'auth.basic'     => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-        'guest'          => 'App\Http\Middleware\RedirectIfAuthenticated',
-        'auth.admin'     => 'Tsawler\Vcms5\Middleware\RedirectIfNotAdminMiddleware',
-        'auth.pages'     => 'Tsawler\Vcms5\Middleware\RedirectIfNotPagesAdminMiddleware',
-        'auth.blogs'     => 'Tsawler\Vcms5\Middleware\RedirectIfNotBlogsAdminMiddleware',
-        'auth.events'    => 'Tsawler\Vcms5\Middleware\RedirectIfNotEventsAdminMiddleware',
-        'auth.news'      => 'Tsawler\Vcms5\Middleware\RedirectIfNotNewsAdminMiddleware',
-        'auth.faqs'      => 'Tsawler\Vcms5\Middleware\RedirectIfNotFaqsAdminMiddleware',
-        'auth.galleries' => 'Tsawler\Vcms5\Middleware\RedirectIfNotGalleriesAdminMiddleware',
-        'auth.menus'     => 'Tsawler\Vcms5\Middleware\RedirectIfNotMenusAdminMiddleware',
-        'auth.users'     => 'Tsawler\Vcms5\Middleware\RedirectIfNotUsersAdminMiddleware',
+        'auth'             => 'App\Http\Middleware\Authenticate',
+        'auth.basic'       => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+        'guest'            => 'App\Http\Middleware\RedirectIfAuthenticated',
+        'auth.admin'       => 'Tsawler\Vcms5\Middleware\RedirectIfNotAdminMiddleware',
+        'auth.pages'       => 'Tsawler\Vcms5\Middleware\RedirectIfNotPagesAdminMiddleware',
+        'auth.blogs'       => 'Tsawler\Vcms5\Middleware\RedirectIfNotBlogsAdminMiddleware',
+        'auth.events'      => 'Tsawler\Vcms5\Middleware\RedirectIfNotEventsAdminMiddleware',
+        'auth.news'        => 'Tsawler\Vcms5\Middleware\RedirectIfNotNewsAdminMiddleware',
+        'auth.faqs'        => 'Tsawler\Vcms5\Middleware\RedirectIfNotFaqsAdminMiddleware',
+        'auth.galleries'   => 'Tsawler\Vcms5\Middleware\RedirectIfNotGalleriesAdminMiddleware',
+        'auth.menus'       => 'Tsawler\Vcms5\Middleware\RedirectIfNotMenusAdminMiddleware',
+        'auth.users'       => 'Tsawler\Vcms5\Middleware\RedirectIfNotUsersAdminMiddleware',
+        'auth.conferences' => 'TApp\Http\Middleware\RedirectIfNotConferencesAdminMiddleware',
     ];
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,35 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-//Route::get('/', function ()
-//{
-//    $conference = \App\Conference::find(1);
-//
-//    return View::make('rnb')
-//        ->with('conference', $conference);
-//});
-//
-//Route::post('/register', function ()
-//{
-//    return "will save form";
-//});
-
 Route::get('/', 'ConferenceRegistrationController@getRegistrationform');
 Route::post('/register', 'ConferenceRegistrationController@postRegistrationform');
 
 Route::post('/queue/push', function(){
     return Queue::marshal();
 });
+
+
 
 Route::controllers([
     'auth'     => 'Auth\AuthController',
