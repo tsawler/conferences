4060c5b175961dda3b8337c97a00be159ad54a11
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-14 16:46:33 -0300

    phone number addition

--- resources/views/emails/confirmation-email-fr.blade.php
+++ resources/views/emails/confirmation-email-fr.blade.php
@@ -1,3 +1,3 @@
 <p>
-    Merci pour votre inscription au Forum AU-DELÀ DES DÉCHETS – Déchets d’emballages et de papier imprimé. Pour toute question au sujet du forum, veuillez communiquer avec nous au 506-454-8473 ou par courriel à <a href="mailto:info@recyclenb.com">info@recyclenb.com</a>. Nous nous réjouissons d’avance de votre participation à ce forum.
+    Merci pour votre inscription au Forum AU-DELÀ DES DÉCHETS – Déchets d’emballages et de papier imprimé. Pour toute question au sujet du forum, veuillez communiquer avec nous au 506-454-8473, Sans Frais :  1-888-322-8473 ou par courriel à <a href="mailto:info@recyclenb.com">info@recyclenb.com</a>. Nous nous réjouissons d’avance de votre participation à ce forum.
 </p>
