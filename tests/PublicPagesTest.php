d81cfd880d5bcca7c0c1438417ff1fb757c61e96
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-14 17:07:52 -0300

    Testing

---
+++ tests/PublicPagesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use bids\Models\Page;
+
+class PublicPagesTest extends TestCase {
+
+    /**
+     * Test the home page
+     *
+     * @return void
+     */
+    public function testHomepage()
+    {
+        $this->call('GET', '/');
+        $this->assertResponseOk();
+    }
+
+
+    /**
+     * Test for a not found page error
+     *
+     * @return void
+     */
+    public function test404()
+    {
+        $this->call('GET', '/dasfasfdasfdasfdsafdasfa');
+        $this->assertResponseStatus(404);
+    }
+
+}
